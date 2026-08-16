0a603bd5b45366ccdfbfdde6702d593c2b7f3ce6
Rahul Kumar <rahulkumar@Rahuls-MacBook-Pro.local>
2026-08-16 22:34:42 +0530

    windows and this keyword

---
+++ demo.js
@@ -0,0 +1,11 @@
+var a = 10;
+function b() {
+    var c = 20;
+}
+
+console.log(a); // 10 by default if you not mention var keyword then it will be global variable
+console.log(window.a); // 10 window global object
+console.log(this.a); // 10 becuase at global level this==window
+console.log(c);
+console.log(window.c); // undefined becuase c is not global variable
+console.log(b) // function b() { var c = 20; } function is also a global variable
