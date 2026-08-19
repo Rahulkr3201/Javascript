ba807826850349ddd6db380e959514893446dabb
Rahul Kumar <rahulkumar@Rahuls-MacBook-Pro.local>
2026-08-19 22:26:15 +0530

    hoisting

--- let-const-var.js
+++ let-const-var.js
@@ -5,13 +5,14 @@
 // var c=30;
 
 
-//redeclare thing
-debugger;
-var a=20;
-var a=300;
-console.log(a); // 300
+// //redeclare thing
+// debugger;git 
+// var a=20;
+// var a=300;
+// console.log(a); // 300
 
 // let,const var all support hoisting but the let and const are in temporal dead zone so you can not access them before initialization  
 
 // now move to the scope for them
 //{} this is block combine multiple statements.
+
