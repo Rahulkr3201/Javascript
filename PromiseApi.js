c206b8276f7d782c74b386e20333bcece59c370d
Rahul Kumar <rahulofficial3200@gmail.com>
2026-08-31 08:41:41 +0530

    promiseApi

---
+++ PromiseApi.js
@@ -0,0 +1,5 @@
+//promise that all take iterator , mostly promise array, and retrurn the promises with the response object arrays.
+//promise.all([p1,p2,p3]) eg,p1 taks 3 sec, pr takes 4 sec, p3 takes 1sec.
+//cases
+//case1:if all passes the whole promise.all return after 4sec.
+//case2:if any one fails(p2 fails ) , the whole promsie fails, regardless of the other rest promise,they can pass or fail ,but the whole promise.all()fails after 4sec.  
